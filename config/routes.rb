edb2552b640e21cc4b850f467b409debd74a8694
Nethelbert <gilblasse@gmail.com>
2020-01-21 01:32:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  resources :coupon, only: [:index, :show, :update, :create, :destroy, :new]
+  resources :coupons, only: [:index, :show, :update, :create, :destroy, :new]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
